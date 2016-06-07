e0644ba0cabe0b8693a6d70ec7e19a715b28e245
Vaccani <Vaccani@DESKTOP-KT2J804>
2016-06-07 19:16:43 -0300

    presentation

---
+++ Api/Controllers/ContaController.cs
@@ -0,0 +1,60 @@
+﻿using Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class ContaController : ApiController
+    {
+        public List<Conta> contas = new List<Conta>();
+
+        public ContaController()
+        {
+            contas.Add(new Conta { idConta = 1, Tipo = Conta.TipoConta.CORRENTE, SituacaoCriacao = Conta.SituacaoCriacaoConta.PENDENTE_APROVACAO, Situacao = Conta.SituacaoConta.VIGENTE, Saldo = 0 });
+            contas.Add(new Conta { idConta = 2, Tipo = Conta.TipoConta.CORRENTE, SituacaoCriacao = Conta.SituacaoCriacaoConta.PENDENTE_APROVACAO, Situacao = Conta.SituacaoConta.VIGENTE, Saldo = 0 });
+            contas.Add(new Conta { idConta = 3, Tipo = Conta.TipoConta.CORRENTE, SituacaoCriacao = Conta.SituacaoCriacaoConta.PENDENTE_APROVACAO, Situacao = Conta.SituacaoConta.VIGENTE, Saldo = 0 });
+            contas.Add(new Conta { idConta = 4, Tipo = Conta.TipoConta.CORRENTE, SituacaoCriacao = Conta.SituacaoCriacaoConta.PENDENTE_APROVACAO, Situacao = Conta.SituacaoConta.VIGENTE, Saldo = 0 });
+        }
+
+        public IEnumerable<Conta> Get()
+        {
+            return contas;
+        }
+
+
+        public IHttpActionResult Get(int id)
+        {
+            var conta = contas.FirstOrDefault((p) => p.idConta == id);
+            if (conta == null)
+            {
+                return NotFound();
+            }
+            return Ok(conta);
+        }
+
+        // POST: api/Default
+        public void Post(Conta value)
+        {
+            if (value != null)
+            {
+                contas.Add(value);
+            }
+        }
+
+        public void Put(int id, Conta value)
+        {
+            if (id > 0 && value != null)
+            {
+                contas.Add(value);
+            }
+        }
+
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Api/Models/Conta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class Conta
+    {
+        
+        public enum TipoConta
+        {
+            POUPANCA, CORRENTE, ELETRONICA, SALARIO, ESPECIAL_ELETRONICA
+        }
+
+        public enum SituacaoCriacaoConta
+        {
+            PENDENTE_APROVACAO, APROVADA, REPROVADA
+        }
+
+        public enum SituacaoConta
+        {
+            VIGENTE, ENCERRADA
+        }
+
+        public int idConta { get; set; }
+        public TipoConta Tipo { get; set; }
+        public SituacaoCriacaoConta SituacaoCriacao { get; set; }
+        public SituacaoConta Situacao { get; set; }
+        public decimal Saldo { get; set; }
+
+    }
+}
