c4eb1860275d656b5a2ee50a89bb9e9d9a4c024c
Vaccani <Vaccani@DESKTOP-KT2J804>
2016-06-07 19:53:49 -0300

    rename

--- Api/Controllers/ContaController.cs
+++ Api/Controllers/ContaController.cs
@@ -1,4 +1,4 @@
-﻿using Presentation.Models;
+﻿using Api.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace Presentation.Controllers
+namespace Api.Controllers
 {
     public class ContaController : ApiController
     {
