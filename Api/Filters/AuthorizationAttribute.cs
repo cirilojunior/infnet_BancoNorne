0b51b7059edd22fc5836115849bf4d38d9d5174a
Vaccani <Vaccani@DESKTOP-KT2J804>
2016-06-13 12:26:15 -0300

    criacao de um filter para controlar todas as requisicoes com jwt

---
+++ Api/Filters/AuthorizationAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Api.Filters
+{
+    public class AuthorizationAttribute : AuthorizationFilterAttribute
+    {
+        public override Task OnAuthorizationAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+
+            return Task.FromResult<object>(null);
+
+        }
+
+    }
+}
